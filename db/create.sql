68a6c5621d04298a6bffc45fc3d55a5da101fa41
tijnwerner <74540053+tijnwerner@users.noreply.github.com>
2022-02-25 09:32:10 +0000

    begin database

--- db/create.sql
+++ db/create.sql
@@ -8,10 +8,10 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2)
+  fromaat NUMERIC
+
 );
 
-
---
 -- populate with data
 --
 -- generared using
@@ -20,10 +20,10 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('condoom klein', 'de perfecte condoom voor de ondergemiddelde man', '816905633-0', 1.50);
-insert into products (name, description, code, price) values ('condoom medium', 'de perfecte condoom voor de gemiddelde man', '077030122-3', 1.75);
-insert into products (name, description, code, price) values ('condoom groot', 'de perfecte condoom voor de bovengemiddelde man (zwarte man)', '445924201-X', 2);
-insert into products (name, description, code, price) values ('extra dunne condoom', 'de onzichtbare condoom, hij bestaat maar je voelt hem niet voor het ontieme orgasme', '693155505-7', 2);
-insert into products (name, description, code, price) values ('glow in the dark condoom', 'voor de ultieme ervaring in het donker met de nieuwe glow in the dark condoom', '686928463-6', 3);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('condoom klein', 'de perfecte condoom voor de ondergemiddelde man', '816905633-0', 1.50,1);
+insert into products (name, description, code, price) values ('condoom medium', 'de perfecte condoom voor de gemiddelde man', '077030122-3', 1.75,2);
+insert into products (name, description, code, price) values ('condoom groot', 'de perfecte condoom voor de bovengemiddelde man (zwarte man)', '445924201-X', 2,3);
+insert into products (name, description, code, price) values ('extra dunne condoom', 'de onzichtbare condoom, hij bestaat maar je voelt hem niet voor het ontieme orgasme', '693155505-7', 2,2);
+insert into products (name, description, code, price) values ('glow in the dark condoom', 'voor de ultieme ervaring in het donker met de nieuwe glow in the dark condoom', '686928463-6', 3,2);
+insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14,2);
 
