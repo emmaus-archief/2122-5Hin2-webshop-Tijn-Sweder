5c4f9aaaa01b53737f7e6596dfe4e17b88ea43bb
SwederMyG <74650285+SwederMyG@users.noreply.github.com>
2022-03-01 15:48:20 +0000

    2de tabel toegevoegd

--- db/create.sql
+++ db/create.sql
@@ -7,11 +7,24 @@ CREATE TABLE products (
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2)
-  tijdsduur NUMERIC(10)
-);
+  price NUMERIC(10, 2),
+  tijdsduur NUMERIC(10),
+  waardering_id INTEGER
+  );
 
--- populate with data
+CREATE TABLE Waardering (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255)
+  );
+  
+  
+  
+
+
+
+  
+  
+  -- populate with data
 --
 -- generared using
 -- curl "https://api.mockaroo.com/api/910b6c20?count=100&key=90eac760" > seed.sql
@@ -19,10 +32,20 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, tijdsduur) values ('GTA V', '', '816905633-0', 1.50,10);
-insert into products (name, description, code, price, tijdsduur) values ('god of war', '', '077030122-3', 1.75,25);
-insert into products (name, description, code, price, tijdsduur) values ('Hearts of iron', '', '445924201-X', 2,3);
-insert into products (name, description, code, price, tijdsduur) values ('RISK', '', '693155505-7', 2,3);
-insert into products (name, description, code, price, tijdsduur) values ('assasins creed black flag', '', '686928463-6', 3,8);
-insert into products (name, description, code, price, tijdsduur) values ('Call of duty black ops 2', '', '492662523-7', 14,9);
+insert into products (name, description, code, price, tijdsduur, waardering_id) values ('GTA V', '', '816905633-0', 1.50,10, 2);
+insert into products (name, description, code, price, tijdsduur, waardering_id) values ('god of war', '', '077030122-3', 1.75,25, 1);
+insert into products (name, description, code, price, tijdsduur, waardering_id) values ('Hearts of iron', '', '445924201-X', 2,3, 4);
+insert into products (name, description, code, price, tijdsduur, waardering_id) values ('RISK', '', '693155505-7', 2,3, 3);
+insert into products (name, description, code, price, tijdsduur, waardering_id) values ('assasins creed black flag', '', '686928463-6', 3,8, 2);
+insert into products (name, description, code, price, tijdsduur, waardering_id) values ('Call of duty black ops 2', '', '492662523-7', 14,9, 3);
 
+
+insert into Waardering (name) values ('goed');
+insert into Waardering (name) values ('oke');
+insert into Waardering (name) values ('meh');
+insert into Waardering (name) values ('slecht');
+
+  
+  
+  
+ 
