94265fa86418310a7ce34db8eeda0344a3cbcdfa
SwederMyG <74650285+SwederMyG@users.noreply.github.com>
2022-03-01 15:22:03 +0000

    onderwerp veranderd en tijdsduur toegevoegd

--- db/create.sql
+++ db/create.sql
@@ -8,8 +8,7 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2)
-  fromaat NUMERIC
-
+  tijdsduur NUMERIC(10)
 );
 
 -- populate with data
@@ -20,10 +19,10 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('condoom klein', 'de perfecte condoom voor de ondergemiddelde man', '816905633-0', 1.50,1);
-insert into products (name, description, code, price) values ('condoom medium', 'de perfecte condoom voor de gemiddelde man', '077030122-3', 1.75,2);
-insert into products (name, description, code, price) values ('condoom groot', 'de perfecte condoom voor de bovengemiddelde man (zwarte man)', '445924201-X', 2,3);
-insert into products (name, description, code, price) values ('extra dunne condoom', 'de onzichtbare condoom, hij bestaat maar je voelt hem niet voor het ontieme orgasme', '693155505-7', 2,2);
-insert into products (name, description, code, price) values ('glow in the dark condoom', 'voor de ultieme ervaring in het donker met de nieuwe glow in the dark condoom', '686928463-6', 3,2);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14,2);
+insert into products (name, description, code, price, tijdsduur) values ('GTA V', '', '816905633-0', 1.50,10);
+insert into products (name, description, code, price, tijdsduur) values ('god of war', '', '077030122-3', 1.75,25);
+insert into products (name, description, code, price, tijdsduur) values ('Hearts of iron', '', '445924201-X', 2,3);
+insert into products (name, description, code, price, tijdsduur) values ('RISK', '', '693155505-7', 2,3);
+insert into products (name, description, code, price, tijdsduur) values ('assasins creed black flag', '', '686928463-6', 3,8);
+insert into products (name, description, code, price, tijdsduur) values ('Call of duty black ops 2', '', '492662523-7', 14,9);
 
